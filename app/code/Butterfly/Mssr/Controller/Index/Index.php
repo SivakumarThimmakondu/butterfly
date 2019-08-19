c6a07a9abf80922d26cbb89a693d272b2dd663d6
pwcdeepa <deepalakshmi.v.tpr@in.pwc.com>
2019-08-19 11:35:09 +0530

    Home page offer& css merged

---
+++ app/code/Butterfly/Mssr/Controller/Index/Index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Butterfly\Mssr\Controller\Index;
+
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+
+class Index extends \Magento\Framework\App\Action\Action implements HttpPostActionInterface
+{
+
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Action\Context  $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+		echo "controller is working";
+		exit;
+
+    }
+}
