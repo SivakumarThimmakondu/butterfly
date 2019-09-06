2a9f65bd5cf00cd9e29c7ad98dc34503b12e541a
pwcdeepa <deepalakshmi.v.tpr@in.pwc.com>
2019-09-06 16:58:58 +0530

    header modification sep6

--- app/etc/config.php
+++ app/etc/config.php
@@ -313,6 +313,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_Worldpay' => 1,
+        'Acton_Calculator' => 1,
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
